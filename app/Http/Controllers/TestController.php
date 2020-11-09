5fc84f1f90183cb4fd3559091a7614dd93ff5d00
sovietoperatives <65005696+junandaip@users.noreply.github.com>
2020-11-09 09:49:22 +0700

    updated TestController to return date from server

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Date;
+
 class TestController extends Controller
 {
     public function index()
     {
         $json = '{"Time":{"date": "2020-11-7 22:01:22.012350", "timezone_type": "7", "timezone": "UTC"}}';
         $jsonString = json_encode($json);
-        echo(json_decode($jsonString));
+        echo (json_decode($jsonString));
+    }
+
+    public function showDate()
+    {
+        $date = Date::all();
+        return response()->json([
+            'Time' => $date
+        ]);
     }
 }
